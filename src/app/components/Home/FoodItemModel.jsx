b9ff0767c029e43c14ec24356a144e3620ca63d1
devindawanasinghe <devindawanasinghe@gmail.com>
2024-12-18 01:46:12 +0530

    foodmodel-new-update

--- src/app/components/Home/FoodItemModel.jsx
+++ src/app/components/Home/FoodItemModel.jsx
@@ -5,10 +5,19 @@ import { FaMinus } from "react-icons/fa";
 import { FaPlus } from "react-icons/fa";
 
 function FoodItemModel({ isOpen, onClose, product }) {
-    const [count, setcount] = useState(0);
+  const [count, setcount] = useState(0);
   const [selectedTops, setSelectedTops] = useState([]);
   const [selectedSides, setSelectedSides] = useState([]);
-  const [total,setTotal] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  // prvent bg scrolling
+  const openModal = () => {
+    document.body.style.overflow = 'hidden';
+  };
+  const closeModal = () => {
+    document.body.style.overflow = '';
+  };
+
   //counter
   const increment = () => {
     if (count < 99) {
@@ -41,81 +50,84 @@ function FoodItemModel({ isOpen, onClose, product }) {
   //total calculation
   useEffect(() => {
     let totalAcc = product.price;
-    if(count === 0){
+    if (count === 0) {
       setTotal(totalAcc);
     }
-        if(selectedTops){
-          selectedTops.forEach((item)=>(
-             totalAcc  += item.price 
-          ))
-        }
-        if(selectedSides){
-          selectedSides.forEach((item)=>(
-             totalAcc += item.price 
-          ))
-        }
-      setTotal(totalAcc * count);
-  }, [count, selectedTops, selectedSides,product.price]);
+    if (selectedTops) {
+      selectedTops.forEach((item) => (totalAcc += item.price));
+    }
+    if (selectedSides) {
+      selectedSides.forEach((item) => (totalAcc += item.price));
+    }
+    setTotal(totalAcc * count);
+  }, [count, selectedTops, selectedSides, product.price]);
 
   const handlAdtoCart = (e) => {
     e.preventDefault();
     e.stopPropagation();
   };
   if (!isOpen) return null;
+  openModal();
+
 
   return (
-    <div
-      className="fixed inset-0 flex items-center justify-center bg-black z-50 bg-opacity-80"
-      
-    >
+  
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
       <div
-        className="bg-white rounded-lg shadow-lg relative px-6 py-2 w-[90%] max-w-md sm:max-w-lg md:w-[600px]  flex flex-col"
+        className="absolute flex flex-col px-6 py-2 bg-white rounded-lg shadow-2xl w-[80%] max-w-[400px] 2xl:max-w-[500px]"
         onClick={(e) => e.stopPropagation()}
       >
         <div className="flex items-center justify-between">
           <button
-            className="text-4xl text-gray-500 hover:text-black"
-            onClick={onClose}
+            className="text-2xl text-gray-500 lg:text-3xl 2xl:text-4xl hover:text-black"
+            onClick={()=> {onClose(); closeModal()}}
           >
             &times;
           </button>
         </div>
-        <p className="font-bold text-[20px]">{product.name}</p>
-        <p className="font-bold text-[15px] text-[#616161]">
+        <p className="font-bold text-[18px] 2xl:text-[20px]">{product.name}</p>
+        <p className="font-bold text-[12px] 2xl:text-[15px] text-[#616161]">
           {product.description}
         </p>
         <div className="flex">
-          <p className="font-bold text-[18px] sm:text-[20px] text-[#616161] inline-flex ">
+          <p className="font-bold text-[16px] 2xl:text-[20px] text-[#616161] inline-flex ">
             {product.ratings}
             <IoStar className="mt-[4px] ml-1 mr-1" />
           </p>
-          <p className="text-[18px] sm:text-[20px] text-[#616161] inline-flex">
-            (200
-            <IoIosAdd className="mt-[5px]" />)
+          <p className="text-[16px] 2xl:text-[20px] text-[#616161] inline-flex font-bold">
+            ({product.ratingsCount}
+            <IoIosAdd className="mt-[4px]" />)
           </p>
-          <p className="font-bold text-[18px] sm:text-[20px] text-[#616161] inline-flex ml-1">
-            {product.price}/=
+          <p className="font-bold text-[16px] 2xl:text-[22px] text-[#616161] inline-flex ml-1">
+            Rs:{product.price}/=
           </p>
         </div>
-        <img src={product.cartImg} alt="product.image" />
+        <img className="" src={product.cartImg} alt="product.image" />
+        
+        {product.customize.top ?
+        <>
+        <p className="font-bold text-[12px] 2xl:text-[15px]">Add on Top</p>
+        <p className="text-[9px] 2xl:text-[13px] text-[#616161] -mt-2">
+          (optional)
+        </p>
+        </>:null}
 
-        <p className="font-bold text-[14px] sm:text-[18px] mt-2">Add on Top</p>
-        <p className="text-[13px] sm:text-[15px] text-[#616161] -mt-2">(optional)</p>
-
-        {product.customize.top.map((item, index) => (
+        {product.customize.top?.map((item, index) => (
           <label key={index} className="block">
-            <div className="flex mt-1">
+            <div className="flex ">
               <input
                 type="checkbox"
                 value={item}
                 onChange={(e) => {
                   handleTopChange(e, item);
                 }}
-                className="w-[22px] h-[22px] sm:w-[26px] sm:h-[26px] mr-2 mt-[3px] border-4 rounded-full bg-gray-100  border-black"
+                className="w-[28px] h-[18px] xs:w-[20px] xs:h-[20px] 2xl:w-[26px] 2xl:h-[26px] mr-2 mt-[3px] border-4 rounded-full bg-gray-100  border-black"
               />
               <div>
-                <p className="font-bold text-[12px] sm:text-[15px] mb-[-2px]">{item.name}</p>
-                <p className=" font-bold sm:text-[12px] text-[10px] text-[#616161]">
+                <p className="font-bold text-[10px] 2xl:text-[12px] mb-[-2px]">
+                  {item.name}
+                </p>
+                <p className=" font-bold text-[10px] 2xl:text-[11px] text-[#616161]">
                   Add a RS.{item.price}/=
                 </p>
               </div>
@@ -123,52 +135,64 @@ function FoodItemModel({ isOpen, onClose, product }) {
           </label>
         ))}
 
-        <p className="font-bold text-[14px] sm:text-[18px] sm-1 sm:mt-2">Add a Side</p>
-        <p className="text-[13px] sm:text-[15px] text-[#616161] -mt-2">(optional)</p>
+          {product.customize.side ? (
+            <>
+              <p className="font-bold text-[12px] 2xl:text-[15px] 2xl-1 2xl:mt-2">
+                Add a Side
+              </p>
+              <p className="text-[9px] 2xl:text-[13px] text-[#616161] -mt-2">
+                (optional)
+              </p>
+            </>
+          ) : null}
+              {product.customize.side?.map((item, index) => (
+                <label key={index} className="block">
+                  <div className="flex 2xl:mt-1 ">
+                    <input
+                      type="checkbox"
+                      value={item}
+                      onChange={(e) => {
+                        handleSideChange(e, item);
+                      }}
+                      className="w-[28px] h-[18px] xs:w-[20px] xs:h-[20px] 2xl:w-[26px] 2xl:h-[26px] mr-2 mt-[3px] border-4 rounded-full bg-gray-100  border-black"
+                    />
+                    <div>
+                      <p className="font-bold text-[10px] 2xl:text-[12px] mb-[-2px]">
+                        {item.name}
+                      </p>
+                      <p className="font-bold text-[10px] 2xl:text-[11px] text-[#616161]">
+                        Add a RS.{item.price}/=
+                      </p>
+                    </div>
+                  </div>
+                </label>
+              ))}
 
-        {product.customize.side.map((item, index) => (
-          <label key={index} className="block">
-            <div className="flex sm:mt-1 ">
-              <input
-                type="checkbox"
-                value={item}
-                onChange={(e) => {
-                  handleSideChange(e, item);
-                }}
-                className="w-[22px] h-[22px] sm:w-[26px] sm:h-[26px] mr-2 mt-[1px] border-4 rounded-full bg-gray-100  border-black "
-              />
-              <div>
-                <p className="font-bold text-[12px] sm:text-[15px] mb-[-2px]">{item.name}</p>
-                <p className="font-bold sm:text-[12px] text-[10px] text-[#616161]">
-                  Add a RS.{item.price}/=
-                </p>
-              </div>
-            </div>
-          </label>
-        ))}
-
-        <div className="flex items-center justify-between">
-          <div className="w-[100px] h-[33px] flex my-4 sm:w-[111px]">
+        <div className="flex items-center justify-between mt-[-3px] ">
+          <div className="w-[100px] h-[30px] flex my-4 2xl:w-[111px] gap-x-[4px] 2xl:gap-x-[6px] ">
             <button
               onClick={(e) => {
-                e.stopPropagation(); 
+                e.stopPropagation();
                 decrement();
               }}
-              className="w-[33px] h-[33px] bg-black rounded-full mr-1 "
+              className="w-[22px] xs:w-[30px] h-[22px] xs:h-[30px]  bg-black rounded-full flex items-center justify-center"
               type="button"
             >
-              <FaMinus color="white" className="mt-[0.5px] sm:ml-[8.6px] ml-[6.9px]" />
+              <FaMinus
+                color="white"
+                className=""
+              />
             </button>
 
             {count < 10 ? (
-              <div className="w-[33px] h-[33px] bg-[#FF4C00] rounded-full mr-1 flex items-center justify-center ">
-                <span className=" text-white text-[20px] mt-[2.5px] mr-[1px]">
+              <div className="w-[22px] xs:w-[30px] h-[22px] xs:h-[30px]  bg-[#FF4C00] rounded-full flex items-center justify-center">
+                <span className=" text-white text-[16px]  xs:text-[20px] ">
                   {count}
                 </span>
               </div>
             ) : (
-              <div className="w-[33px] h-[33px] bg-[#FF4C00] rounded-full mr-1 flex items-center justify-center">
-                <span className=" text-white text-[20px]">
+              <div className="w-[22px] xs:w-[30px] h-[22px] xs:h-[30px]  bg-[#FF4C00] rounded-full flex items-center justify-center">
+                <span className=" text-white text-[16px]  xs:text-[20px]">
                   {count}
                 </span>
               </div>
@@ -176,13 +200,16 @@ function FoodItemModel({ isOpen, onClose, product }) {
 
             <button
               onClick={(e) => {
-                e.stopPropagation(); 
+                e.stopPropagation();
                 increment();
               }}
-              className="w-[33px] h-[33px] bg-[#FF4C00] rounded-full mr-1 "
+              className="w-[22px] xs:w-[30px] h-[22px] xs:h-[30px]  bg-[#FF4C00] rounded-full flex items-center justify-center  "
               type="button"
             >
-              <FaPlus className=" sm:ml-[9px] ml-[7px]" color="white" />
+              <FaPlus
+                className=""
+                color="white"
+              />
             </button>
           </div>
 
@@ -190,44 +217,27 @@ function FoodItemModel({ isOpen, onClose, product }) {
             <button
               onClick={handlAdtoCart}
               type="button"
-              className="bg-[#FF4C00] px-2 sm:px-0 text-white font-bold rounded-full h-[30px] sm:h-[40px] w-full sm:w-[180px] md:w-[200px] lg:w-[230px] shadow-lg hover:bg-[#E54300] flex items-center justify-center"
+              className="bg-[#FF4C00] px-2 2xl:px-0 text-white font-bold rounded-full h-[24px] 2xl:h-[33px] w-[120px] xs:w-[160px] 2xl:w-[180px] md:w-[200px] shadow-2xl hover:bg-[#E54300] flex items-center justify-center xs:h-[30px]"
             >
-              {total>0 ? null : <span className=" font-bold text-[10px] sm:text-[15px] lg:w-[103px]">
-                Add to Cart -
-              </span>
-               }
-              <span className=" py-1 text-white rounded-lg sm:text-[15px] text-[10px]">
-                RS.
+              {total > 0 && window.innerWidth < 640 ? 
+                  <span className=" font-bold text-[10px] xs:text-[12px] 2xl:text-[15px] 2xl:w-[103px]">
+                  Add - 
+                  </span>: (
+                  <span className=" font-bold text-[10px] xs:text-[12px] 2xl:text-[15px] 2xl:w-[103px]">
+                  Add to Cart -
+                  </span>
+              )}
+              <span className=" py-1 text-white rounded-2xl 2xl:text-[15px] text-[10px] xs:text-[12px]">
+                Rs.
                 {total}
                 /=
               </span>
             </button>
           </div>
         </div>
-
-       {/* testing checking items
-        <div className="mt-4">
-          <p className="font-bold">Selected Tops:</p>
-          <ul>
-            {selectedTops.map((top, index) => (
-              <li key={index}>
-                {top.name} - RS.{top.price}/=
-              </li>
-            ))}
-          </ul>
-
-          <p className="font-bold">Selected Sides:</p>
-          <ul>
-            {selectedSides.map((side, index) => (
-              <li key={index}>
-                {side.name} - RS.{side.price}/=
-              </li>
-            ))}
-          </ul>
-        </div>*/}
       </div>
     </div>
-  )
+  );
 }
 
-export default FoodItemModel
+export default FoodItemModel;
