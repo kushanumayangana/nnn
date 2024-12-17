3d0b7289dc955e7ab13af83720f02706d039221e
devindawanasinghe <devindawanasinghe@gmail.com>
2024-12-18 00:13:12 +0530

    app.js routing added.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from '../app/Home/page';
 import ModelTest from "../app/AuthModels/ModelTest";
+import Payment from "../app/components/Payment/PaymentSuccess";
+import PaymentFaild from "../app/components/Payment/PaymentFailed";
+import OrderStatus from "../app/components/OrderStatus/OrderStatus";
+
+
 
 const Page = () => {
   return (
@@ -9,6 +14,12 @@ const Page = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/modeltest" element={< ModelTest />} />
+        <Route path="/payment" element={< Payment />} />
+        <Route path="/paymentfaild" element={< PaymentFaild />} />
+        <Route path="/order" element={< OrderStatus />} />
+
+
+
       </Routes>
     </Router>
   );
