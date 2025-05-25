2c48ec315a35907b9aeb890b277342a02d8de069
devindawanasinghe <devindawanasinghe@gmail.com>
2025-05-26 01:25:49 +0530

    chnages

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-     <p className='text-orange-400'>fhhhdddd</p>
+     <p className='text-orange-400'>Devinda</p>
     </div>
   )
 }
